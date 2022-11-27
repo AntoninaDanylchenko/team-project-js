b97017b13586bef10a8370f43c2c350b18318805
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2022-11-27 18:57:25 +0200

    add

--- src/js/references/references.js
+++ src/js/references/references.js
@@ -3,5 +3,9 @@ export const refs = {
   closeModalBtn: document.querySelector('[data-action="close-modal"]'),
   backdrop: document.querySelector('.js-backdrop'),
   galleryEl: document.querySelector(".gallery"),
+  pagination: document.querySelector(".pagination"),
+  paginationList: document.querySelector(".pagination_list"),
+  decrementBtn: document.querySelector('[data-decrement]'),
+  incrementBtn: document.querySelector('[data-increment]'),
 };
 
