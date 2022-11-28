d1c69caac0d1a80871d8aa081cfad46cba24dcfe
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2022-11-28 23:38:35 +0200

    debug fix

--- src/js/libraryMain.js
+++
@@ -1,6 +0,0 @@
-import { catchFilmId } from './localStorage/localStorage.js';
-import { createMarkupLibrary } from './components/createMarkupLibrary.js';
-// console.log(catchFilmId(436270));
-// function () {
-
-// }
