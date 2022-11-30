8c7e18529265d882858c6836277f375acc7e8352
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2022-11-30 23:59:06 +0200

    update scroll

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,28 +1,28 @@
- winddow.onLoad = () => {
-    window.onscroll = function (e) {
-        let winY = window.scrollY;
-        if (winY > 300) {
-            winY = null;
-        }
-        };
-        
-const scrollBtn = document.querySelector('.isShowBtn');
+const scrollBtn = document.getElementById('scrollUpLink');
 
-     window.onscroll = () => {
-        if (window.scrollY > 700) {
-            scrollBtn.classList.remove('isShowBtn_hide');
-        } else if (window.scrollY < 700) {
-             scrollBtn.classList.add('isShowBtn_hide');
-        }
-    };
+window.onscroll = () => {
+  if (window.scrollY < 700) {
+      scrollBtn.classList.remove('isShowBtn');
+      scrollBtn.classList.add('isShowBtn_hide');
+  } else if (window.scrollY > 700) {
+      scrollBtn.classList.add('isShowBtn');
+    scrollBtn.classList.remove('isShowBtn_hide');
+  }
+};
 
-    scrollBtn.onclick = () => {
-        window.scrollTo(0, 0);
+scrollBtn.onclick = () => {
+  window.scrollTo(0, 0);
+};
+
+window.onLoad = () => {
+  window.onscroll = function (e) {
+    let winY = window.scrollY;
+    if (winY > 300) {
+      winY = null;
     }
-}
-
-
-   
+  };
+};
+    
 
 
 
