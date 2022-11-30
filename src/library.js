ff42b8f23e3bdf317cecd9dadd6fffb8d17f36c2
Bohatynska Oksana <98977553+Bohatynska@users.noreply.github.com>
2022-11-30 10:43:04 +0000

    add changes on library

--- src/library.js
+++ src/library.js
@@ -4,23 +4,22 @@ import { queue } from './js/queue/queue.js';
 import { libraryMain } from './js/libraryMain.js';
 import { createMarkupLibrary } from './js/components/createMarkupLibrary.js';
 import { refs } from './js/references/references.js';
+
 refs.btnQueue.addEventListener('click', drawMyQueue);
 function drawMyQueue() {
   // refs.galleryLibraryEl.innerHTML = '';
-  const filmInLocal = JSON.parse(localStorage.getItem('Add-to-watched'));
+  const filmInLocal = JSON.parse(localStorage.getItem('Add-to-queue'));
   console.log(filmInLocal);
   const draw = filmInLocal
     .map(
       item => `<div class="library-wraper">
-    
-    <a
+        <a
       class='library-gallery__item'
       href='#'
       target='_blank'
       rel='noopener noreferrer'
     >
       
-
         <img
           src='https://image.tmdb.org/t/p/w500${item.poster_path}'
           class='library-card__img'
@@ -39,8 +38,10 @@ function drawMyQueue() {
   
   </div>`
     )
-    .join();
-  console.log(draw);
+
+    .join(' ');
+
+  // console.log(draw);
 
   refs.galleryLibraryEl.innerHTML = draw;
 }
@@ -49,8 +50,8 @@ refs.btnWatched.addEventListener('click', drawMyWatched);
 function drawMyWatched() {
   // refs.galleryLibraryEl.innerHTML = '';
 
-  const filmInLocal = JSON.parse(localStorage.getItem('Add-to-queue'));
-  console.log(filmInLocal);
+  const filmInLocal = JSON.parse(localStorage.getItem('Add-to-watched'));
+  // console.log(filmInLocal);
   const draw = filmInLocal
     .map(
       item => `<div class="library-wraper">
@@ -74,15 +75,13 @@ function drawMyWatched() {
           <div class='wrap-library'>
             <p class='library-info__genre'>${item.genre_id} | ${item.release_date}</p>
          <span class ="film-rating">${item.vote_average}</span>
-          </div>
-       
-      </div>
+          </div></div>
     </a>
   
   </div>`
     )
-    .join();
-  console.log();
+    .join(' ');
+
   refs.galleryLibraryEl.innerHTML = draw;
 }
 drawMyWatched();
