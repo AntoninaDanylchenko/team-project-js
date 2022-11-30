dcf67d426c81360009d91d85373c12ae5263eb57
Nataliia <maksimeyko@ukr.net>
2022-11-30 08:33:00 +0200

    update scc

--- src/js/components/info-user.js
+++ src/js/components/info-user.js
@@ -3,12 +3,9 @@ import { refs } from '../references/references.js';
 
    
 const markup = users.map(user => {return `<li class='list'>
-   
- <img class="item__img" src="${user.photo}" alt="${user.name}" width="170"
+    <img class="item__img" src="${user.photo}" alt="${user.name}" width="170"
       height="160" >
-      
-
-  <div class='list__wrapper'>
+        <div class='list__wrapper'>
   <p class='list__title'>${user.name}</p>
   <p class='list__subtitle'>${user.position}</p>
   <ul class='social-media'>
@@ -16,14 +13,14 @@ const markup = users.map(user => {return `<li class='list'>
   <a class="social-media__link" href="${user.socialmedia}"
   rel="noopener noreferrer" aria-label="linkedin">
   <svg class="social-media__icon" width="20" height="20">
-    <use href="./images/symbol-defs.svg#icon-github"></use>
+    <use href="https://cdn4.iconfinder.com/data/icons/social-media-free-13/32/Linkedin_social_media_logo-512.png"></use>
   </svg></a>
   </li>
   <li class='social-media__item'>
   <a class="social-media__link" href="${user.github}"
   rel="noopener noreferrer" aria-label="github">
   <svg class="social-media__icon" width="20" height="20">
-    <use href="./images/team-photo/symbol-defs.svg#icon-github"></use>
+    <use href="http://www.w3.org/2000/svg"></use>
   </svg></a>
   </li>
   </ul>
@@ -33,12 +30,4 @@ const markup = users.map(user => {return `<li class='list'>
  refs.container.insertAdjacentHTML('beforeend', markup.join(''));
  console.log(markup);
 
-//  const imageBase = document.createElement('img');
-//  imageBase.setAttribute('src', 'images/team-photo/AndriyL.jpg');
-//  imageBase.setAttribute("width", '170');
-//  imageBase.setAttribute('height', '160');
-// //  imageBase.setAttribute('alt', `${user.name}`);
-// console.log(imageBase);
 
-
-// document.querySelector('.list').appendChild(imageBase);
--- src/js/components/photo-team.js
+++
@@ -1,6 +0,0 @@
-
-export const photo = [{
-ant: './images/team-photo/Antonina.webp',
-antJpg:'./images/team-photo/Antonina.webp',
-},
-]
