3dae4aa017b9a85c37218e52e1cb570bbbf125e0
TosiaDan <antonina.ivanets@gmail.com>
2022-12-01 23:23:37 +0100

    create hbs template for modal

--- src/js/components/createMarkupGalleryCards.js
+++ src/js/components/createMarkupGalleryCards.js
@@ -2,11 +2,7 @@ import { refs } from '../references/references';
 import templateCard from '../../templates/templateCard.hbs';
 
 function createResultMarkup(results) {
-  // console.log(results);
-  // console.log(templateCard(results));
-
   refs.galleryEl.innerHTML = templateCard(results);
-  // console.log
 }
 
 export { createResultMarkup };
