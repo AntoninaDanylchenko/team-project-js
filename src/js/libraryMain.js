f8a5c678fb724fa3dc9e8c87addc7ea1a38fe35d
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2022-12-02 12:45:26 +0200

    add noCard function

--- src/js/libraryMain.js
+++ src/js/libraryMain.js
@@ -1,11 +1,55 @@
+import { Notify } from 'notiflix';
 import { refs } from './references/references.js';
+
+
+
+// function clearTextOfLocalStorage(){
+//   const filmInLocalQ = JSON.parse(localStorage.getItem('Add-to-queue'));
+//   const filmInLocalW = JSON.parse(localStorage.getItem('Add-to-watched'));
+//     if(filmInLocalQ || !filmInLocalQ.length){
+//       refs.galleryLibraryEl.innerHTML = "You didn`t add any film";
+
+//     }
+//    if(!filmInLocalW || !filmInLocalW.length){
+//     refs.galleryLibraryEl.innerHTML = "You didn`t add any film";
+
+//     }
+//    }
+// clearTextOfLocalStorage();
+
+function noCardinLocalStorage(){
+  // if(refs.galleryLibraryEl.closest('.no-card')){
+  //   Notify.failure("exit ");
+  //   return;
+  // }
+// const noCardLibraryInfo = document.createElement('h2');
+// noCardLibraryInfo.textContent = "You didn`t add any film to  this position";
+// noCardLibraryInfo.classList.add("no-card");
+
+// refs.noCard.append(noCardLibraryInfo);
+refs.noCard.classList.remove('visually-hidden');
+
+}
+
+
+
+
 
 refs.btnQueue.addEventListener('click', drawMyQueue);
 function drawMyQueue(item) {
-  // refs.galleryLibraryEl.innerHTML = '';
+  refs.galleryLibraryEl.innerHTML = '';
   const filmInLocal = JSON.parse(localStorage.getItem('Add-to-queue'));
-  console.log(filmInLocal);
-  console.log(filmInLocal[0].genres);
+
+  if(!filmInLocal || !filmInLocal.length){
+    refs.noCard.classList.remove('visually-hidden');
+  return ;
+  }
+// Notify.warning("add hidden ")
+  refs.noCard.classList.add('visually-hidden');
+
+
+  // console.log(filmInLocal);
+  // console.log(filmInLocal[0].genres);
   const draw = filmInLocal
     .map(item => {
       const genreFilmsArr = item.genres.map(genre => genre.name);
@@ -41,18 +85,27 @@ function drawMyQueue(item) {
       </div>`;
     })
     .join(' ');
-  console.log(draw);
+  // console.log(draw);
   refs.galleryLibraryEl.innerHTML = draw;
   //   drawMyQueue();
 }
 
 refs.btnWatched.addEventListener('click', drawMyWatched);
 function drawMyWatched(item) {
-  // refs.galleryLibraryEl.innerHTML = '';
+  refs.galleryLibraryEl.innerHTML = '';
 
   const filmInLocal = JSON.parse(localStorage.getItem('Add-to-watched'));
-  console.log(filmInLocal);
-  console.log(filmInLocal[0].genres);
+  if(!filmInLocal || !filmInLocal.length){
+
+   refs.noCard.classList.remove('visually-hidden');
+    return;
+  }
+  // Notify.warning('add hidden')
+  refs.noCard.classList.add('visually-hidden');
+  
+
+  // console.log(filmInLocal);
+  // console.log(filmInLocal[0].genres);
   const draw = filmInLocal
     .map(item => {
       const genreFilmsArr = item.genres.map(genre => genre.name);
@@ -88,7 +141,9 @@ function drawMyWatched(item) {
       </div>`;
     })
     .join(' ');
-  console.log(draw);
+  // console.log(draw);
   refs.galleryLibraryEl.innerHTML = draw;
+ 
 }
 drawMyWatched();
+
